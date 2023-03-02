bb062ae4eebc7a9885b1b5af3f4a110e9f3978ee
dandelion16012 <shchekinapa@yandex.ru>
2023-03-02 11:12:33 +0300

    Fixed2 cpp file

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
- 
+using namespace std; 
 int main(int argc, char** argv){
  string name;
- std::cin >> name;
- std::cout << "Hello world from " << name << std::endl;
+cin >> name;
+cout << "Hello world from " << name << std::endl;
 }
 
 
